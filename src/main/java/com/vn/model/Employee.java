74f448b7149da66fcebc05776ca58b0ae7590a9c
trungvietri82 <trungvietri82@gmail.com>
2024-02-02 16:21:27 +0700

    T
    dfadf
    
    Co-Authored-By: trungvietri82 <39473992+trungvietri82@users.noreply.github.com>

---
+++ src/main/java/com/vn/model/Employee.java
@@ -0,0 +1,82 @@
+package com.vn.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String descripTions;
+	private String diengiai;
+	public Employee() {
+
+	}
+
+	public Employee(String firstName, String lastName, String emailId) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	@Column(name = "first_name", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Column(name = "last_name", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Column(name = "email_address", nullable = false)
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getDescripTions() {
+		return descripTions;
+	}
+
+	public void setDescripTions(String descripTions) {
+		this.descripTions = descripTions;
+	}
+
+	public String getDiengiai() {
+		return diengiai;
+	}
+
+	public void setDiengiai(String diengiai) {
+		this.diengiai = diengiai;
+	}
+}
---
+++ src/main/java/com/vn/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.vn.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vn.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
